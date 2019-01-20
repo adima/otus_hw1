9830a33c772f80b665b278bc1e4ffd6ee404b0c8
dmitry <firtray@gmail.com>
2019-01-20 21:35:07 +0300

    making project tree as asked added configs

--- src/log_analyzer.py
+++ src/log_analyzer.py
@@ -8,9 +8,7 @@ import gzip
 import argparse
 import json
 import logging
-import unittest
 from copy import copy
-import shutil
 
 from string import Template
 
@@ -334,32 +332,6 @@ def check_and_clear_test_folders(test_config):
     for p in paths:
         fls = os.listdir(p)
         [os.remove(os.path.join(p, f)) for f in fls]
-
-
-class ResultTest(unittest.TestCase):
-    def test_works_ok_if_no_logs(self):
-        """
-        Проверяет, работает ли пайплайн, если в папке нет файлов для обработки
-        :return:
-        """
-        config = get_config('config_test')
-        check_and_clear_test_folders(config)
-        log_dir = config['LOG_DIR']
-        res = choose_log(log_dir)
-        self.assertTrue(res is None)
-
-    def test_works_ok_with_gz(self):
-        """
-        Проверяет, отрабатывает ли пайплайн на файлах .gz
-        :return:
-        """
-        config = get_config('config_test')
-        check_and_clear_test_folders(config)
-        log_dir = config['LOG_DIR']
-        shutil.copy2('log_source/nginx-access-ui.log-20170630.gz', log_dir)
-        main(config, smoke_test=True)
-        res = os.listdir(config['REPORT_DIR'])
-        self.assertTrue('report-2017.06.30.html' in res)
 
 
 if __name__ == "__main__":
