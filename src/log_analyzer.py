07114b6984fa5f3fbf2b1e29b34fac27fe53c911
dmitry <firtray@gmail.com>
2019-11-19 14:57:40 +0300

    moved tests to test.py

--- src/log_analyzer.py
+++ src/log_analyzer.py
@@ -247,38 +247,10 @@ def check_and_clear_test_folders(test_config):
         for fl in fls:
             os.remove(os.path.join(path, fl))
 
-
-class ResultTest(unittest.TestCase):
-    def test_works_ok_if_no_logs(self):
-        """
-        Проверяет, работает ли пайплайн, если в папке нет файлов для обработки
-        :return:
-        """
-        config = get_config('config_test')
-        check_and_clear_test_folders(config)
-        log_dir = config['LOG_DIR']
-        res = choose_log(log_dir)
-        self.assertTrue(res is None)
-
-    def test_works_ok_with_gz(self):
-        """
-        Проверяет, отрабатывает ли пайплайн на файлах .gz
-        :return:
-        """
-        config = get_config('config_test')
-        check_and_clear_test_folders(config)
-        log_dir = config['LOG_DIR']
-        shutil.copy2('log_source/nginx-access-ui.log-20170630.gz', log_dir)
-        main(config, smoke_test=True)
-        res = os.listdir(config['REPORT_DIR'])
-        self.assertTrue('report-2017.06.30.html' in res)
-
-
-
 if __name__ == "__main__":
-    unittest.main()
     arg_parser = argparse.ArgumentParser(description='Log analyzer arguments')
     arg_parser.add_argument("--config", type=str, default='./config')
     args = arg_parser.parse_args()
     config_loc = get_config(config, args.config)
-    main(config_loc, smoke_test=False)
+    main(config_loc, smoke_test=True)
+
